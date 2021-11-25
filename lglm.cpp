vWernay/lua

commit 3dfe948d32234d82471c81c752e43593bb74512c

refactor: small cleanup
feat: empty vector constructors
refactor: small optimization/non-pessimization and logic-restructuring pass
refactor(glm/binding): outline default metatable replacement

lglm.cpp
@@ -1182,23 +1182,21 @@ LUA_API lua_Integer glm_tohash(lua_State *L, int idx, int ignore_case) {
 LUA_API int glmVec_dot(lua_State *L) {
   const TValue *x = glm_index2value(L, 1);
   const TValue *y = glm_index2value(L, 2);
-  if (ttisinteger(x) && ttisinteger(y))
-    lua_pushinteger(L, ivalue(x) * ivalue(y));
-  else if (ttisnumber(x) && ttisnumber(y))
-    lua_pushnumber(L, nvalue(x) * nvalue(y));
-  else if (ttisquat(x) && ttisquat(y))
-    lua_pushnumber(L, cast_num(glm::dot(glm_qvalue(x), glm_qvalue(y))));
-  else if (ttisvector(x) && ttypetag(x) == ttypetag(y)) {
+  if (ttypetag(x) == ttypetag(y)) {
     switch (ttypetag(x)) {
+      case LUA_VNUMINT: lua_pushinteger(L, ivalue(x) * ivalue(y)); break;
+      case LUA_VNUMFLT: lua_pushnumber(L, nvalue(x) * nvalue(y)); break;
       case LUA_VVECTOR2: lua_pushnumber(L, cast_num(glm::dot(glm_v2value(x), glm_v2value(y)))); break;
       case LUA_VVECTOR3: lua_pushnumber(L, cast_num(glm::dot(glm_v3value(x), glm_v3value(y)))); break;
       case LUA_VVECTOR4: lua_pushnumber(L, cast_num(glm::dot(glm_v4value(x), glm_v4value(y)))); break;
+      case LUA_VQUAT: lua_pushnumber(L, cast_num(glm::dot(glm_qvalue(x), glm_qvalue(y)))); break;
       default: {
-        lua_pushnumber(L, 0);
-        break;
+        return luaL_typeerror(L, 1, LABEL_NUMBER " or " LABEL_VECTOR " type");
       }
     }
   }
+  else if (ttisnumber(x) && ttisnumber(y))  // number-coercion
+    lua_pushnumber(L, nvalue(x) * nvalue(y));
   else {
     return luaL_typeerror(L, 1, LABEL_NUMBER " or " LABEL_VECTOR " type");
   }
@@ -1647,9 +1645,13 @@ static int glm_createVector(lua_State *L, glm::length_t desiredSize = 0) {
 
   // If the vector is of a fixed/desired size and only one non-table argument has been supplied
   const int top = _gettop(L);
-  const TValue *o_f = glm_index2value(L, 1);
-  if (desiredSize > 0 && top == 1 && glm_castvalue(o_f, v.x)) {
-    return glm_pushvec(L, glmVector(glm::vec<4, T>(v.x)), desiredSize);
+  if (desiredSize > 0) {
+    if (top == 0)
+      return glm_pushvec(L, glmVector(v), desiredSize);
+    if (top == 1 && glm_castvalue(glm_index2value(L, 1), v.x)) {
+      v.y = v.z = v.w = v.x;
+      return glm_pushvec(L, glmVector(v), desiredSize);
+    }
   }
 
   // Maximum number of stack values to parse, starting from "idx"
@@ -1664,7 +1666,7 @@ static int glm_createVector(lua_State *L, glm::length_t desiredSize = 0) {
   if (desiredSize == 0 && v_len == 0)
     return luaL_error(L, LABEL_VECTOR " requires 1 to 4 values");
   else if (desiredSize != 0 && v_len != desiredSize)
-    return luaL_error(L, LABEL_VECTOR "%d requires 1 or %d values", cast_int(desiredSize), cast_int(desiredSize));
+    return luaL_error(L, LABEL_VECTOR "%d requires 0, 1 or %d values", cast_int(desiredSize), cast_int(desiredSize));
   else if (v_len == 1) {
     GLM_IF_CONSTEXPR(std::is_same<T, bool>::value)
       lua_pushboolean(L, cast_int(v.x));
@@ -2143,6 +2145,10 @@ static int vec_trybinTM(lua_State *L, const TValue *p1, const TValue *p2, StkId 
     case TM_BXOR: INT_VECTOR_OPERATION(operator^, res, v, p2); break;
     case TM_SHL: INT_VECTOR_OPERATION(operator<<, res, v, p2); break;
     case TM_SHR: INT_VECTOR_OPERATION(operator>>, res, v, p2); break;
+    case TM_BNOT: {
+      glm_setvvalue2s(res, operator~(cast_vec4(v.v4, lua_Integer)), tt_p1);
+      return 1;
+    }
     default: {
       break;
     }

libs/glm-binding/api.hpp
@@ -21,6 +21,8 @@
 @@ LUAGLM_ALIASES Include aliases (e.g., length vs. magnitude) to functions.
 **
 ** Features:
+@@ LUAGLM_INSTALL_METATABLES Update the global metatables for vector and matrix,
+**  types, if ones are not already defined, with lglmlib on load.
 @@ LUAGLM_REPLACE_MATH Force replace _G.math with the binding library on open.
 @@ LUAGLM_RECYCLE Enable object recycling: trailing/unused parameters in a
 **  function call, e.g., matrix objects, are used as a result store.
@@ -1773,8 +1775,8 @@ TRAITS_LAYOUT_DEFN(nlz, glm::nlz, LAYOUT_UNARY, gLuaTrait<unsigned>)
   const Tr::safe::type v3 = Tr::safe::Next(LB);                 \
   const Tr::value_type v4 = Tr::value_trait::Next(LB);          \
   if (glm::intersectLineSphere(v1, v2, v3, v4, v5, v6, v7, v8)) \
-    TRAITS_PUSH(LB, v5, v6, v7, v8);                            \
-  return gLuaBase::Push(LB);                                    \
+    TRAITS_PUSH(LB, true, v5, v6, v7, v8);                      \
+  TRAITS_PUSH(LB, false);                                       \
   LUA_MLM_END
 
 #define LAYOUT_INTERSECT_RAY_PLANE(LB, F, Tr, ...) \
@@ -1785,8 +1787,8 @@ TRAITS_LAYOUT_DEFN(nlz, glm::nlz, LAYOUT_UNARY, gLuaTrait<unsigned>)
   const Tr::safe::type v3 = Tr::safe::Next(LB);    \
   const Tr::safe::type v4 = Tr::safe::Next(LB);    \
   if (glm::intersectRayPlane(v1, v2, v3, v4, v5))  \
-    return gLuaBase::Push(LB, v5);                 \
-  return gLuaBase::Push(LB);                       \
+    TRAITS_PUSH(LB, true, v5);                     \
+  TRAITS_PUSH(LB, false);                          \
   LUA_MLM_END
 
 #define LAYOUT_INTERSECT_RAY_SPHERE(LB, F, Tr, ...)       \
@@ -1797,8 +1799,8 @@ TRAITS_LAYOUT_DEFN(nlz, glm::nlz, LAYOUT_UNARY, gLuaTrait<unsigned>)
   const Tr::safe::type v3 = Tr::safe::Next(LB);           \
   const Tr::value_type v4 = Tr::value_trait::Next(LB);    \
   if (glm::intersectRaySphere(v1, v2, v3, v4, v5, v6))    \
-    TRAITS_PUSH(LB, v5, v6);                              \
-  return gLuaBase::Push(LB);                              \
+    TRAITS_PUSH(LB, true, v5, v6);                        \
+  TRAITS_PUSH(LB, false);                                 \
   LUA_MLM_END
 
 NUMBER_VECTOR_DEFN(intersectLineSphere, glm::intersectLineSphere, LAYOUT_INTERSECT_LINE_SPHERE)
@@ -1813,8 +1815,8 @@ GLM_BINDING_QUALIFIER(intersectLineTriangle) {
   const gLuaVec3<float>::type v4 = gLuaVec3<>::Next(LB);
   const gLuaVec3<float>::type v5 = gLuaVec3<>::Next(LB);
   if (glm::intersectLineTriangle(v1, v2, v3, v4, v5, v6))
-    return gLuaBase::Push(LB, v6);
-  return gLuaBase::Push(LB);
+    TRAITS_PUSH(LB, true, v6);
+  TRAITS_PUSH(LB, false);
   GLM_BINDING_END
 }
 
@@ -1828,8 +1830,8 @@ GLM_BINDING_QUALIFIER(intersectRayTriangle) {
   const gLuaVec3<>::type v1 = gLuaVec3<>::Next(LB);
   const gLuaVec3<>::type v2 = gLuaVec3<>::Next(LB);
   if (glm::intersectRayTriangle(orig, dir, v0, v1, v2, baryPosition, distance))
-    TRAITS_PUSH(LB, baryPosition, distance);
-  return gLuaBase::Push(LB);
+    TRAITS_PUSH(LB, true, baryPosition, distance);
+  TRAITS_PUSH(LB, false);
   GLM_BINDING_END
 }
 #endif

CMakeLists.txt
@@ -496,7 +496,7 @@ OPTION(LUAGLM_APICHECK
   keys/key-types. Otherwise, follow tables and set the result to nil" OFF
 )
 
-OPTION(LUAGLM_COMPAT_IPAIRS "Reintroduce compatibility for the ``__ipairs`` metamethod that was deprecated in 5.3 and removed in 5.4" ON)
+OPTION(LUAGLM_COMPAT_IPAIRS "Reintroduce compatibility for the __ipairs metamethod that was deprecated in 5.3 and removed in 5.4" OFF)
 OPTION(LUAGLM_EXT_DEFER "Enable the defer statement" OFF)
 OPTION(LUAGLM_EXT_DEFER_OLD "Import func2close from ltests.h into the base library as `defer`" ON)
 OPTION(LUAGLM_EXT_JOAAT "Enable compile time Jenkins' one-at-a-time hashing" ON)

libs/glm-binding/lglmlib.cpp
@@ -43,6 +43,11 @@
   #include "geom.hpp"
 #endif
 
+/* @TEMP: The GLM library will be force-installed by default */
+#if !defined(LUAGLM_INSTALL_METATABLES)
+  #define LUAGLM_INSTALL_METATABLES
+#endif
+
 /* Create a "{ function, glm_function }" registry instance */
 #define REG_STR(C) #C
 #define GLM_LUA_REG(NAME) \
@@ -230,12 +235,14 @@ extern "C" {
 #endif
 
     /* Setup default metatables */
+#if defined(LUAGLM_INSTALL_METATABLES)
     lua_lock(L);
     if (G(L)->mt[LUA_TVECTOR] == GLM_NULLPTR)
       G(L)->mt[LUA_TVECTOR] = hvalue(s2v(L->top - 1));
     if (G(L)->mt[LUA_TMATRIX] == GLM_NULLPTR)
       G(L)->mt[LUA_TMATRIX] = hvalue(s2v(L->top - 1));
     lua_unlock(L);
+#endif
 
     return 1;
   }

ltm.c
@@ -80,6 +80,7 @@ const TValue *luaT_gettmbyobj (lua_State *L, const TValue *o, TMS event) {
       break;
     default:
       mt = G(L)->mt[ttype(o)];
+      break;
   }
   return (mt ? luaH_getshortstr(mt, G(L)->tmname[event]) : &G(L)->nilvalue);
 }
@@ -148,26 +149,36 @@ static int callbinTM (lua_State *L, const TValue *p1, const TValue *p2,
 
 void luaT_trybinTM (lua_State *L, const TValue *p1, const TValue *p2,
                     StkId res, TMS event) {
-  if (l_likely(callbinTM(L, p1, p2, res, event))) { /* FALLTHROUGH */ }
-  else if (tmbitop(event)) {
-    if (ttisnumber(p1) && ttisnumber(p2))
-      luaG_tointerror(L, p1, p2);
-    /*
-    ** @NOTE: bitwise operators apply to integer vectors (i.e., glm::ivec). As
-    ** this iteration of Lua does not explicitly support integer vectors, each
-    ** vector is int-casted beforehand.
-    */
-    else if (ttisvector(p1) && glm_trybinTM(L, p1, p2, res, event)) {
-      /* FALLTHROUGH; successful operation */
+  /*
+  ** For performance reasons, inlined unary/binary vector/quaternion/matrix
+  ** operators take precedence over metamethods. This approach leaves the base
+  ** Lua implementation in tact.
+  **
+  ** As bitwise operators only apply to integer vectors, i.e., glm::ivec. This
+  ** iteration of LuaGLM will int-cast each vector component beforehand. Native
+  ** integer-vector support is TODO.
+  **
+  ** @TODO: Document above.
+  */
+  if (ttisvector(p1) || ttismatrix(p1) || ttisvector(p2) || ttismatrix(p2)) {
+    if (l_likely(glm_trybinTM(L, p1, p2, res, event))) {
+      return;
     }
-    else
-      luaG_opinterror(L, p1, p2, "perform bitwise operation on");
   }
-  else if (!glm_trybinTM(L, p1, p2, res, event)) {
-    if (ttisvector(p1) || ttismatrix(p1))
-      luaG_opinterror(L, p1, p2, "perform unsupported operation on");
-    else
-      luaG_opinterror(L, p1, p2, "perform arithmetic on");
+
+  if (l_unlikely(!callbinTM(L, p1, p2, res, event))) {
+    switch (event) {
+      case TM_BAND: case TM_BOR: case TM_BXOR:
+      case TM_SHL: case TM_SHR: case TM_BNOT: {
+        if (ttisnumber(p1) && ttisnumber(p2))
+          luaG_tointerror(L, p1, p2);
+        else
+          luaG_opinterror(L, p1, p2, "perform bitwise operation on");
+      }
+      /* calls never return, but to avoid warnings: *//* FALLTHROUGH */
+      default:
+        luaG_opinterror(L, p1, p2, "perform arithmetic on");
+    }
   }
 }
 

lvm.c
@@ -1296,15 +1296,13 @@ LUA_JUMPTABLE_ATTRIBUTE void luaV_execute (lua_State *L, CallInfo *ci) {
         TValue *rb = vRB(i);
         TValue *rc = vRC(i);
         if (ttisvector(rb)) {  /* fast track for integers / character indexing? */
-          if (ttisinteger(rc) && glmVec_fastgeti(rb, ivalue(rc), ra)) { /* nothing */ }
-          else if (ttisstring(rc) && glmVec_fastgets(rb, tsvalue(rc), ra)) { /* nothing */ }
-          else {
+          if (!(ttisinteger(rc) && glmVec_fastgeti(rb, ivalue(rc), ra))
+              && !(ttisstring(rc) && glmVec_fastgets(rb, tsvalue(rc), ra))) {
             Protect(glmVec_get(L, rb, rc, ra));
           }
         }
         else if (ttismatrix(rb)) {  /* fast track for integers? */
-          if (ttisinteger(rc) && glmMat_fastgeti(rb, ivalue(rc), ra)) { /* nothing */ }
-          else {
+          if (!(ttisinteger(rc) && glmMat_fastgeti(rb, ivalue(rc), ra))) {
             Protect(glmMat_get(L, rb, rc, ra));
           }
         }

makefile
@@ -95,7 +95,6 @@ SYSLIBS=
 
 LUA_PATCHES = -DLUA_C99_MATHLIB  \
 		-DLUA_USE_LONGJMP \
-		-DLUAGLM_COMPAT_IPAIRS \
 		-DLUAGLM_EXT_DEFER_OLD \
 		-DLUAGLM_EXT_COMPOUND \
 		-DLUAGLM_EXT_INTABLE \
@@ -110,6 +109,7 @@ LUA_PATCHES = -DLUA_C99_MATHLIB  \
 		-DLUAGLM_EXT_BLOB \
 		-DLUAGLM_EXT_READLINE_HISTORY \
 		# -DLUAGLM_EXT_READONLY \
+		# -DLUAGLM_COMPAT_IPAIRS \
 
 GLM_FLAGS = -DLUAGLM_LIBVERSION=999 \
 		-DGLM_FORCE_INTRINSICS \

commit 1e1d1765799582c42af9fe46e289fa2d52068b89

fix: things
This commit also includes:

fix: constructor regression with vec1 constructors, introduced in 3dfe948d3.
fix(glm/binding): matrix_access_hpp
tweak(glm/binding): cleanup backwards compatibility
tweak: begin glmvec_hash rework; the current implementation is a hack and inefficient.

lglm.cpp
@@ -1402,6 +1402,20 @@ static bool PopulateMatrix(lua_State *L, int idx, int top, bool fixed_size, glmM
 }
 
 /// <summary>
+/// glm::vec<1, ...> is represented by a Lua value.
+/// </summary>
+template<typename T>
+static LUA_INLINE int glm_pushvalue(lua_State *L, const T &v) {
+  GLM_IF_CONSTEXPR(std::is_same<T, bool>::value)
+    lua_pushboolean(L, static_cast<int>(v));
+  else GLM_IF_CONSTEXPR(std::is_integral<T>::value)
+    lua_pushinteger(L, static_cast<lua_Integer>(v));
+  else
+    lua_pushnumber(L, static_cast<lua_Number>(v));
+  return 1;
+}
+
+/// <summary>
 /// Generic vector population/construction function.
 ///
 /// This function will iterate over the current Lua stack and unpack its values;
@@ -1428,6 +1442,10 @@ static int glm_createVector(lua_State *L, glm::length_t desiredSize = 0) {
     if (top == 0)
       return glm_pushvec(L, glmVector(v), desiredSize);
     if (top == 1 && glm_castvalue(glm_index2value(L, 1), v.x)) {
+      if (desiredSize == 1) {
+        return glm_pushvalue<T>(L, v.x);
+      }
+
       v.y = v.z = v.w = v.x;
       return glm_pushvec(L, glmVector(v), desiredSize);
     }
@@ -1447,13 +1465,7 @@ static int glm_createVector(lua_State *L, glm::length_t desiredSize = 0) {
   else if (desiredSize != 0 && v_len != desiredSize)
     return luaL_error(L, GLM_STRING_VECTOR "%d requires 0, 1 or %d values", cast_int(desiredSize), cast_int(desiredSize));
   else if (v_len == 1) {
-    GLM_IF_CONSTEXPR(std::is_same<T, bool>::value)
-      lua_pushboolean(L, static_cast<int>(v.x));
-    else GLM_IF_CONSTEXPR(std::is_integral<T>::value)
-      lua_pushinteger(L, static_cast<lua_Integer>(v.x));
-    else
-      lua_pushnumber(L, static_cast<lua_Number>(v.x));
-    return 1;
+    return glm_pushvalue<T>(L, v.x);
   }
   return glm_pushvec(L, glmVector(v), v_len);
 }

lglm_string.hpp
@@ -351,66 +351,89 @@ namespace detail {
 
 namespace glm {
 namespace hash {
-#include <cmath>
 
-#if !defined(l_hashfloat)
   /// <summary>
-  /// ltable.c: l_hashfloat
+  /// Temporary solution as the previous implementation was slow. A collection
+  /// of 'spatial hashing' algorithms exist for vector-type structures that
+  /// should be considered.
+  ///
+  /// @TODO: Better
   /// </summary>
-  static GLM_FUNC_QUALIFIER int l_hashfloat(lua_Number n) {
-    int i;
-    lua_Integer ni = 0;
-    n = std::frexp(n, &i) * -cast_num(INT_MIN);
-    if (!lua_numbertointeger(n, &ni)) { /* is 'n' inf/-inf/NaN? */
-      lua_assert(luai_numisnan(n) || l_mathop(fabs)(n) == cast_num(HUGE_VAL));
-      return 0;
-    }
-    else { /* normal case */
-      unsigned int u = cast_uint(i) + cast_uint(ni);
-      return cast_int(u <= cast_uint(INT_MAX) ? u : ~u);
-    }
-  }
-#endif
+  template<typename T>
+  struct lglm_hash {
+    GLM_FUNC_DECL size_t operator()(const T& n) const;
+  };
 
-  static GLM_INLINE void hash_combine(size_t &seed, size_t hash) {
+  template<>
+  struct lglm_hash<double> {
+    GLM_FUNC_QUALIFIER size_t operator()(const double& n) const {
+      union { double __t; size_t __a; } __scalar_hash;
+      __scalar_hash.__a = 0;
+      __scalar_hash.__t = n;
+      return (n == 0.0) ? 0 : __scalar_hash.__a;  // -/+ 0.0 should return same hash.
+    }
+  };
+
+  template<>
+  struct lglm_hash<float> {
+    GLM_FUNC_QUALIFIER size_t operator()(const float& n) const {
+      union { float __t; size_t __a; } __scalar_hash;
+      __scalar_hash.__a = 0;
+      __scalar_hash.__t = n;
+      return (n == 0.f) ? 0 : __scalar_hash.__a;  // -/+ 0.0 should return same hash.
+    }
+  };
+
+  /// <summary>
+  /// glm::detail::hash_combine
+  /// </summary>
+  static GLM_INLINE void lglm_hashcombine(size_t &seed, size_t hash) {
     hash += 0x9e3779b9 + (seed << 6) + (seed >> 2);
     seed ^= hash;
   }
 
   template<typename T, glm::qualifier Q>
   GLM_FUNC_QUALIFIER typename std::enable_if<std::is_floating_point<T>::value, size_t>::type hash(glm::vec<2, T, Q> const &v) {
+    const lglm_hash<T> hasher;
+
     size_t seed = 0;
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.x)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.y)));
+    lglm_hashcombine(seed, hasher(v.x));
+    lglm_hashcombine(seed, hasher(v.y));
     return seed;
   }
 
   template<typename T, glm::qualifier Q>
   GLM_FUNC_QUALIFIER typename std::enable_if<std::is_floating_point<T>::value, size_t>::type hash(glm::vec<3, T, Q> const &v) {
+    const lglm_hash<T> hasher;
+
     size_t seed = 0;
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.x)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.y)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.z)));
+    lglm_hashcombine(seed, hasher(v.x));
+    lglm_hashcombine(seed, hasher(v.y));
+    lglm_hashcombine(seed, hasher(v.z));
     return seed;
   }
 
   template<typename T, glm::qualifier Q>
   GLM_FUNC_QUALIFIER typename std::enable_if<std::is_floating_point<T>::value, size_t>::type hash(glm::vec<4, T, Q> const &v) {
+    const lglm_hash<T> hasher;
+
     size_t seed = 0;
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.x)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.y)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.z)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.w)));
+    lglm_hashcombine(seed, hasher(v.x));
+    lglm_hashcombine(seed, hasher(v.y));
+    lglm_hashcombine(seed, hasher(v.z));
+    lglm_hashcombine(seed, hasher(v.w));
     return seed;
   }
 
   template<typename T, glm::qualifier Q>
   GLM_FUNC_QUALIFIER typename std::enable_if<std::is_floating_point<T>::value, size_t>::type hash(glm::qua<T, Q> const &v) {
+    const lglm_hash<T> hasher;
+
     size_t seed = 0;
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.x)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.y)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.z)));
-    hash_combine(seed, l_hashfloat(static_cast<lua_Number>(v.w)));
+    lglm_hashcombine(seed, hasher(v.x));
+    lglm_hashcombine(seed, hasher(v.y));
+    lglm_hashcombine(seed, hasher(v.z));
+    lglm_hashcombine(seed, hasher(v.w));
     return seed;
   }
 }

libs/glm-binding/api.hpp
@@ -926,9 +926,23 @@ TRAITS_DEFN(rayPicking, glm::rayPicking, gLuaVec3<>, gLuaVec3<>, gLuaFloat, gLua
 TRAITS_LAYOUT_DEFN(containsProjection, glm::containsProjection, LAYOUT_BINARY_EPS, gLuaMat4x4<>) /* LUA_MATRIX_EXTENSIONS */
 #endif
 
-#if defined(GTC_MATRIX_ACCESS_HPP) /* Zero Based */
-MATRIX_DEFN(column, glm::__column, LAYOUT_UNARY, gLuaTrait<glm::length_t>)
-MATRIX_DEFN(row, glm::__row, LAYOUT_UNARY, gLuaTrait<glm::length_t>)
+#if defined(GTC_MATRIX_ACCESS_HPP) /* @NOTE: Zero Based */
+#define LAYOUT_MATRIX_ACCESS_COLUMN(LB, F, Tr, ...) \
+  LAYOUT_MATRIX_ACCESS(LB, F, Tr, Tr::col_type, Tr::row_type, gLuaTrait<glm::length_t>)
+#define LAYOUT_MATRIX_ACCESS_ROW(LB, F, Tr, ...) \
+  LAYOUT_MATRIX_ACCESS(LB, F, Tr, Tr::row_type, Tr::col_type, gLuaTrait<glm::length_t>)
+#define LAYOUT_MATRIX_ACCESS(LB, F, Tr, TrComp, TrDims, TrIdx)                        \
+  LUA_MLM_BEGIN                                                                       \
+  glm::length_t __idx = cast(glm::length_t, luaL_checkinteger((LB).L, (LB).idx + 1)); \
+  if (__idx < 0 || __idx >= TrDims::length())                                         \
+    return luaL_argerror((LB).L, (LB).idx + 1, "matrix index");                       \
+  else if (TrComp::Is(LB, (LB).idx + 2)) /* Set */                                    \
+    TRAITS_FUNC(LB, F, Tr, TrIdx, TrComp);                                            \
+  TRAITS_FUNC(LB, F, Tr, TrIdx); /* Get */                                            \
+  LUA_MLM_END
+
+MATRIX_DEFN(column, glm::column, LAYOUT_MATRIX_ACCESS_COLUMN)
+MATRIX_DEFN(row, glm::row, LAYOUT_MATRIX_ACCESS_ROW)
 #endif
 
 #if defined(GTC_MATRIX_INVERSE_HPP)
@@ -1448,7 +1462,7 @@ GLM_BINDING_QUALIFIER(pow) {
   GLM_BINDING_BEGIN
   if (gLuaInteger::Is(LB, LB.idx) && gLuaTrait<unsigned>::Is(LB, LB.idx + 1))
     TRAITS_FUNC(LB, glm::pow, gLuaInteger, gLuaTrait<unsigned>);
-  PARSE_NUMBER_VECTOR_QUAT(LB, glm::pow, LAYOUT_BINARY_SCALAR, LAYOUT_BINARY_SCALAR, LAYOUT_BINARY_SCALAR);
+  PARSE_NUMBER_VECTOR_QUAT(LB, glm::pow, LAYOUT_BINARY_SCALAR, LAYOUT_BINARY_OPTIONAL, LAYOUT_BINARY_SCALAR);
   GLM_BINDING_END
 }
 
@@ -2010,12 +2024,16 @@ TRAITS_BINARY_LAYOUT_DEFN(rotateZ, glm::rotateZ, LAYOUT_BINARY_SCALAR, gLuaVec3<
 #endif
 
 #if defined(GTX_ROTATE_VECTOR_HPP) || defined(EXT_QUATERNION_COMMON_HPP)
+#if GLM_VERSION >= 998  /* @COMPAT slerp + 'additional spin count' introduced in 0.9.9.8 */
 #define LAYOUT_QUAT_SLERP(LB, F, Tr, ...)                                                                     \
   LUA_MLM_BEGIN                                                                                               \
   if (gLuaTrait<int>::Is((LB), (LB).idx + 3))                                                                 \
     VA_NARGS_CALL_OVERLOAD(TRAITS_FUNC, LB, F, Tr, Tr::safe, Tr::value_trait, gLuaTrait<int>, ##__VA_ARGS__); \
   VA_NARGS_CALL_OVERLOAD(TRAITS_FUNC, LB, F, Tr, Tr::safe, Tr::value_trait, ##__VA_ARGS__);                   \
   LUA_MLM_END
+#else
+  #define LAYOUT_QUAT_SLERP(LB, F, Tr, ...) LAYOUT_TERNARY_SCALAR(LB, F, Tr, ##__VA_ARGS__)
+#endif
 
 NUMBER_VECTOR_QUAT_DEFNS(slerp, glm::__slerp, LAYOUT_TERNARY_SCALAR, LAYOUT_TERNARY_SCALAR, LAYOUT_QUAT_SLERP)
 NUMBER_VECTOR_QUAT_DEFN(barycentric, glm::barycentric, LAYOUT_BARYCENTRIC) /* LUA_VECTOR_EXTENSIONS */

libs/glm-binding/bindings.hpp
@@ -858,6 +858,8 @@ template<typename T, bool FastPath = false>
 struct gLuaPrimitive : gLuaSharedTrait<T, T> {
   using safe = gLuaPrimitive<T, false>;  // @SafeBinding
   using fast = gLuaPrimitive<T, true>;  // @UnsafeBinding
+
+  static GLM_CONSTEXPR glm::length_t length() { return 1; }
   LUA_TRAIT_QUALIFIER T Next(gLuaBase &LB) {
     T v(0);
     gLuaBase::Pull<T, FastPath>(LB, LB.idx++, v);
@@ -892,6 +894,7 @@ struct gLuaTrait<glm::qua<T>, FastPath> : gLuaSharedTrait<T, glm::qua<T>> {
   using safe = gLuaTrait<glm::qua<T>, false>;  // @SafeBinding
   using fast = gLuaTrait<glm::qua<T>, true>;  // @UnsafeBinding
 
+  static GLM_CONSTEXPR glm::length_t length() { return 4; }  // glm::qua<T>::length()
   static GLM_CONSTEXPR const char *Label() { return GLM_STRING_QUATERN; }
 
   LUA_TRAIT_QUALIFIER GLM_CONSTEXPR glm::qua<T> zero() {
@@ -932,6 +935,13 @@ struct gLuaTrait<glm::vec<D, T>, FastPath> : gLuaSharedTrait<T, glm::vec<D, T>> 
   /// Alternative name: lhs_mat_type
   /// </summary>
   using row_type = gLuaTrait<glm::vec<D, T>>;
+
+  /// <summary>
+  /// Trait definition of glm::vec<D,T>::length()
+  /// </summary>
+  static GLM_CONSTEXPR glm::length_t length() {
+    return D;
+  }
 
   static GLM_CONSTEXPR const char *Label() {
     switch (D) {
@@ -991,6 +1001,13 @@ struct gLuaTrait<glm::mat<C, R, T>, FastPath> : gLuaSharedTrait<T, glm::mat<C, R
   /// </summary>
   template<glm::length_t RNext>
   using rhs_mat_type = gLuaTrait<glm::mat<RNext, C, T>>;
+
+  /// <summary>
+  /// Trait definition of glm::mat<C, R, T>::length()
+  /// </summary>
+  static GLM_CONSTEXPR glm::length_t length() {
+    return C;
+  }
 
   static GLM_CONSTEXPR const char *Label() {
     switch (LUAGLM_MATRIX_TYPE(C, R)) {

libs/glm-binding/ext/matrix_extensions.hpp
@@ -692,28 +692,6 @@ namespace glm {
   }
 
   /// <summary>
-  /// Fail-safe glm::row
-  /// </summary>
-  template<typename genType>
-  GLM_FUNC_QUALIFIER typename genType::row_type __row(genType const &m, length_t index) {
-    typename genType::row_type Result(0);
-    if (index >= 0 && index < m[0].length()) {
-      for (length_t i = 0; i < m.length(); ++i) {
-        Result[i] = m[i][index];
-      }
-    }
-    return Result;
-  }
-
-  /// <summary>
-  /// Fail-safe glm::column
-  /// </summary>
-  template<typename genType>
-  GLM_FUNC_QUALIFIER typename genType::col_type __column(genType const &m, length_t index) {
-    return (index >= 0 && index < m.length()) ? m[index] : typename genType::col_type(0);
-  }
-
-  /// <summary>
   /// @TODO
   /// </summary>
   template<typename T, qualifier Q>

libs/glm-binding/ext/vector_extensions.hpp
@@ -24,6 +24,11 @@
 #include <glm/gtx/projection.hpp>
 #include <glm/gtx/spline.hpp>
 #include <glm/gtx/vector_angle.hpp>
+#if GLM_VERSION < 998  /* @COMPAT ext/scalar_common.hpp introduced in 0.9.9.8 */
+#include <glm/gtx/extended_min_max.hpp>
+#elif GLM_VERSION == 998
+#include <glm/ext/scalar_common.hpp>
+#endif
 
 #if GLM_MESSAGES == GLM_ENABLE && !defined(GLM_EXT_INCLUDED)
 #	pragma message("GLM: GLM_EXT_vector_ext extension included")

libs/glm-binding/lglmlib.hpp
@@ -8,7 +8,7 @@
 #define lglmlib_hpp
 
 #define LUAGLM_NAME "lua-glm"
-#define LUAGLM_VERSION "lua-glm 0.2.13"
+#define LUAGLM_VERSION "lua-glm 0.2.14"
 #define LUAGLM_COPYRIGHT "Copyright (C) 2020, Gottfried Leibniz"
 #define LUAGLM_DESCRIPTION "glm bindings for Lua"
 

luac.c
@@ -39,19 +39,19 @@ static const char* output=Output;	/* actual output file name */
 static const char* progname=PROGNAME;	/* actual program name */
 static TString **tmname;
 
-static void fatal(const char* message)
+static l_noret fatal(const char* message)
 {
  fprintf(stderr,"%s: %s\n",progname,message);
  exit(EXIT_FAILURE);
 }
 
-static void cannot(const char* what)
+static l_noret cannot(const char* what)
 {
  fprintf(stderr,"%s: cannot %s %s: %s\n",progname,what,output,strerror(errno));
  exit(EXIT_FAILURE);
 }
 
-static void usage(const char* message)
+static l_noret usage(const char* message)
 {
  if (*message=='-')
   fprintf(stderr,"%s: unrecognized option '%s'\n",progname,message);